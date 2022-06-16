08fbef4571d561c3259cd0e5ace647c354a2b0b8
Biplob123121 <biplobkb123@gmail.com>
2022-06-16 22:37:25 +0600

    initialize origin cloud project

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Login from '../Login/Login';
+
+const Home = () => {
+    return (
+        <section>
+            <div>
+                <h2>Welcome to <strong>Origin Cloud Technologies</strong> </h2>
+            </div>
+            <div>
+                <Login />
+            </div>
+        </section>
+    );
+};
+
+export default Home;
